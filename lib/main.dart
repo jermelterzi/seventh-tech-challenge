0b03bf1404a60d4a0ea9a459bc8892708a990ada
João Victor Terzi <jv_ermel@hotmail.com>
2023-03-20 08:10:22 -0300

    feat: implementing dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:video_monitoring_seventh/src/app/seventh_app.dart';
+import 'package:video_monitoring_seventh/src/core/dependency_injection/dependency_injection.dart'
+    as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await di.init();
+
   runApp(const SeventhApp());
 }
